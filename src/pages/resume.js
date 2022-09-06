d1ff6a83c9665a06a5818da7eee90e3a3eebf545
Greg Stevens <gitgreg@stevens.pro>
2022-09-06 09:08:36 -0600

    feat(content): 📝 /resume: Re-layout, center text

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,82 +1,76 @@
-// http://localhost:8000/career
-
 import React from 'react';
-import { Link } from 'gatsby';
+
 import Layout from '../components/Layout';
+import { Link } from 'gatsby';
+
 import config from '../../config';
 
-import githubSocialImage from '../assets/images/greg-stevens-career-socialify.png';
+import pic1 from '../assets/images/pic01.jpg';
+import pic2 from '../assets/images/pic02.jpg';
+import pic3 from '../assets/images/pic03.jpg';
+import pic4 from '../assets/images/pic04.jpg';
+import pic5 from '../assets/images/pic05.jpg';
+import banner from '../assets/images/banner.jpg';
+const IndexPage = () => <Layout>
+	<article id="main">
+		<header>
+      <h2 style={{textTransform: "none"}}>resume.stevens.pro</h2>
+      <p><span>🚀</span> Launched Aug 2022</p>
+		</header>
+		<section className="wrapper style5" style={{textAlign: "center"}}>
+			<div className="inner">
 
+				<section>
+					<div className="row">
+						<div className="col-6 col-12-medium">
 
-// import { CareerLink } from '../components/CareerLink';
-const RESUME_LINK_TEXT = <code>https://<b>resume.stevens.pro</b>/</code>
-const RESUME_LINK = <Link
-                      to={config.RESUME_URL}
-                      target="_blank"
-                    >{RESUME_LINK_TEXT}</Link>
+              <h3>
+                <Link
+                  to={config.RESUME_URL}
+                  className="button"
+                  >🌐 View Resume Website</Link>
+              </h3>
 
-const IndexPage = () => (
-  <Layout fullMenu>
-    <article id="main">
-      <header>
+						</div>
+						<div className="col-6 col-12-medium">
+              <h3>
+                <Link
+                  to={config.RESUME_PDF_URL}
+                  className="button"
+                  >💼 Download PDF Resume</Link>
+              </h3>
+						</div>
+					</div>
+				</section>
 
-        <h2 style={{textTransform: "none"}}>resume.stevens.pro</h2>
+        <section>
+          <div>
+            <h3>About JSON Resume Schema</h3>
+            <p>The Open Source power behind the scenes</p>
+            <div class="row">
+              <div className="col-6 col-12-medium">
+                <p>
+                  <Link
+                    to="https://www.jsonresume.org/"
+                    target="_blank"
+                  >🎓 Learn About the JSON Resume Schema</Link>
+                </p>
+              </div>
+              <div className="col-6 col-12-medium">
+                <p>
+                  <Link
+                    to={config.RESUME_JSON_URL}
+                  >🤓 View Source JSON File on GitHub</Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
 
-        <p style={{marginTop: "2rem"}}>
-          <Link
-            to={config.RESUME_URL}
-          >🌐 View Resume Website</Link>
-        </p>
+			</div>
+		</section>
+	</article>
 
-        <p style={{marginTop: "2rem"}}>
-          <Link
-            to={config.RESUME_PDF_URL}
-          >💼 Download PDF Resume</Link>
-        </p>
-
-
-{/*
-        <p style={{marginTop: "3rem"}}>
-          <em>
-            Powered by &nbsp;
-            <a href="https://www.jsonresume.org" target="_blank">
-              JSON Resume
-            </a>
-          </em>
-        </p> */}
-
-        <br clear="all" />
-      </header>
-
-    {/* ---- end header --- */}
-
-    <section>
-      <div class="text-center">
-        <p>🚀 Launched Aug 2022</p>
-      </div>
-
-      <h3>vvv Geek Stuff Below vvv</h3>
-
-      <p style={{marginTop: "2rem"}}>
-        <Link
-          to={config.RESUME_JSON_URL}
-        >🤓 View Resume JSON File on GitHub</Link>
-      </p>
-
-      <p style={{marginTop: "2rem"}}>
-        <Link
-          to="https://www.jsonresume.org/"
-          target="_blank"
-        >🎓 Learn About the JSON Resume Schema</Link>
-      </p>
-
-    </section>
-
-
-
-    </article>
-
-  </Layout>
-);
+</Layout>;
 
 export default IndexPage;
